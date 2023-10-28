9bb76638a94ccb0b3858d6e4a356c3ea9bcbbdf7
angus <angusmiguel21@gmail.com>
2023-10-29 00:48:06 +0100

    List

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { List, ListItem, ListIcon, Checkbox } from "@chakra-ui/react";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
+			<List spacing={3}>
+				<ListItem>
+					<ListIcon
+						as={() => <Checkbox onChange={console.log} />}
+						color="green.500"
 					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit
+				</ListItem>
+				<ListItem>
+					<ListIcon as={Checkbox} color="green.500" />
+					Assumenda, quia temporibus eveniet a libero incidunt
+					suscipit
+				</ListItem>
+				<ListItem>
+					<ListIcon as={Checkbox} color="green.500" />
+					Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
+				</ListItem>
+				{/* You can also use custom icons from react-icons */}
+				<ListItem>
+					<ListIcon as={Checkbox} color="green.500" />
+					Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
+				</ListItem>
+			</List>
 		</>
 	);
 }
